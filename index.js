58dc9aff03ad65114ceffa4bd383ff176d2a0a67
NetiSanthoshKumar <santhoshkumarnethi544@gmail.com>
2025-06-04 09:51:27 +0530

    updated

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 import cors from "cors";
-
+dotenv.config();
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
+
+const MONGODB_URI = process.env.MONGODB_URI
 
 const app = express();
 app.use(cors());
@@ -17,4 +20,4 @@ app.use("/orders",orderRouter)
 app.listen(8080, () => {
   mongoose.connect("mongodb://localhost:27017/gcet");
   console.log("Server Started");
-});
+}); 
