47bdba1d6ff2c14261380541ca4a91c179b6ca0f
NetiSanthoshKumar <santhoshkumarnethi544@gmail.com>
2025-06-06 14:08:40 +0530

    updated

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 const orderSchema = mongoose.Schema({
   email: { type: String },
-  orderValue: { type: Number },
-  orderDate: { type: Date,default: Date.now },
+  price: { type: Number },
+  // orderDate: { type: Date,default: Date.now },
 });
-// const product = mongoose.model("Product", productSchema);
+// const product = mongoose.model("Product", );
 
 export default mongoose.model("Order", orderSchema);
