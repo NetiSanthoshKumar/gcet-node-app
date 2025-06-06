2c0c8ff88848b6ed5b186d5974dec80a93dc5b18
NetiSanthoshKumar <santhoshkumarnethi544@gmail.com>
2025-06-06 15:36:06 +0530

    udated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,17 @@
-
 import express from "express";
 import userModel from "../models/userModel.js";
-
+import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
+const SECRET_KEY ="hellowworld";
 const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res) => {
   const { name, email, pass } = req.body;
+  const hashpassword = await bcrypt.hash(pass,10);
   const result = await userModel.insertOne({
     name: name,
     email: email,
-    pass: pass,
+    pass: hashpassword,
   });
   return res.json(result);
 });
@@ -18,7 +20,13 @@ userRouter.post("/login", async (req, res) => {
   const { email, pass } = req.body;
   const result = await userModel.findOne({ email, pass });
   if (!result) return res.json({ message: "Invalid user or password" });
-  return res.json(result);
+  const matchPassword = await bcrypt.compare(pass,result.pass);
+  if(!matchPassword){
+    return res.status(400).json({message:"Invalid Password"});
+  }
+  const token = jwt.sign({email: result.email, id: result._id},SECRET-KEY);
+  console.log(result);
+  return res.json({user : result,token : token});
 });
 
 
